f44cdf5834bf7334d1d26ae48cfeed5c36f1e855
triveni gavathe <gavathe9975@gmail.com>
2026-03-21 21:46:04 +0530

    html files add for the authrcation login etc

--- myresumeanalyzer/resumeAnalyzer/urls.py
+++ myresumeanalyzer/resumeAnalyzer/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns=[
     path('',home,name='home'),
+    path('resume_analyzer/',resume_analyzer,name='resume_analyzer')
 ]
--- myresumeanalyzer/resumeAnalyzer/views.py
+++ myresumeanalyzer/resumeAnalyzer/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def resume_analyzer(request):
+    return render(request,'resume_analyzer.html')
