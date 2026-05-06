321a4b133f0683823457111b6e904d565b805663
triveni gavathe <gavathe9975@gmail.com>
2026-05-06 14:12:03 +0530

    cleanup: remove old project folders and unused files

--- myresumeanalyzer/jobautomation/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobautomationConfig(AppConfig):
-    name = 'jobautomation'
--- myresumeanalyzer/jobautomation/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns=[
-    path('job_home',job_home,name='job_home'),
-]
